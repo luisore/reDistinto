bf9bd73400713048b518dc9ceb3097190c6d58e8
utnso <utnso@ubuntu-server>
2018-04-21 10:25:56 -0300

    conexion con isntancia

--- Coordinador/src/Coordinador.c
+++ Coordinador/src/Coordinador.c
@@ -1,69 +1,123 @@
 #include "Coordinador.h"
-
-int readConfig(char* configFile) {
-	if (configFile == NULL) {
-		return -1;
-	}
-	t_config *config = config_create(configFile);
-	log_info(console_log, " .:: Cargando settings ::.");
-
-	if (config != NULL) {
-		coordinador_setup.PUERTO_ESCUCHA_CONEXIONES = config_get_int_value(
-				config, "PUERTO_ESCUCHA_CONEXIONES");
-		coordinador_setup.ALGORITMO_DISTRIBUCION = config_get_int_value(config,
-				"ALGORITMO_DISTRIBUCION");
-		coordinador_setup.CANTIDAD_ENTRADAS = config_get_int_value(config,
-				"CANTIDAD_ENTRADAS");
-		coordinador_setup.TAMANIO_ENTRADA_BYTES = config_get_int_value(config,
-				"TAMANIO_ENTRADA_BYTES");
-		coordinador_setup.RETARDO_MS = config_get_int_value(config,
-				"RETARDO_MS");
-	}
-	config_destroy(config);
-	return 0;
-}
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+//int readConfig(char* configFile) {
+//	if (configFile == NULL) {
+//		return -1;
+//	}
+//	t_config *config = config_create(configFile);
+//	log_info(console_log, " .:: Cargando settings ::.");
+//
+//	if (config != NULL) {
+//		coordinador_setup.PUERTO_ESCUCHA_CONEXIONES = config_get_int_value(
+//				config, "PUERTO_ESCUCHA_CONEXIONES");
+//		coordinador_setup.ALGORITMO_DISTRIBUCION = config_get_int_value(config,
+//				"ALGORITMO_DISTRIBUCION");
+//		coordinador_setup.CANTIDAD_ENTRADAS = config_get_int_value(config,
+//				"CANTIDAD_ENTRADAS");
+//		coordinador_setup.TAMANIO_ENTRADA_BYTES = config_get_int_value(config,
+//				"TAMANIO_ENTRADA_BYTES");
+//		coordinador_setup.RETARDO_MS = config_get_int_value(config,
+//				"RETARDO_MS");
+//	}
+//	config_destroy(config);
+//	return 0;
+//}
 
 int main(void) {
-	console_log = log_create("coordinador.log", "ReDistinto-Coordinador",
-	true, LOG_LEVEL_TRACE);
-	printf("\n\t\e[31;1m=========================================\e[0m\n");
-	printf("\t.:: Bievenido a ReDistinto ::.");
-	printf("\n\t\e[31;1m=========================================\e[0m\n\n");
-	if (readConfig(PATH_FILE_NAME) < 0) {
-		log_error(console_log, "No se encontró el archivo de configuración");
-		return -1;
+	struct sockaddr_in servidorConfig;
+	servidorConfig.sin_family = AF_INET;
+	servidorConfig.sin_addr.s_addr = INADDR_ANY;
+	servidorConfig.sin_port = htons(8080);
+//	struct sockaddr_in direcciónServidor;
+//		direcciónServidor.sin_family = AF_INET;
+//		direcciónServidor.sin_addr.s_addr = INADDR_ANY;
+//		direcciónServidor.sin_port = htons(8080);
+
+		int servidor = socket(AF_INET, SOCK_STREAM, 0);
+
+		int activado = 1;
+		setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+
+		if (bind(servidor, (void*) &servidorConfig, sizeof(servidorConfig)) != 0) {
+			perror("Falló el bind");
+			return 1;
+		}
+
+		printf("Estoy escuchando\n");
+		listen(servidor, 100);
+
+		//------------------------------
+		struct sockaddr_in clienteConfig;
+//		struct sockaddr_in direcciónCliente;
+		unsigned int direccion;
+//		unsigned int tamañoDirección;
+		int cliente = accept(servidor, (void*) &clienteConfig, &direccion);
+
+		printf("Recibí una conexión en %d!!\n", cliente);
+		send(cliente, "Hola NetCat!", 13, 0);
+		send(cliente, ":)\n", 4, 0);
+
+		//------------------------------
+
+		char* buffer = malloc(50);
+//
+//		while (1) {
+//			int bytesRecibidos = recv(cliente, buffer, 1000, 0);
+//			if (bytesRecibidos <= 0) {
+//				perror("El chabón se desconectó o bla.");
+//				return 1;
+//			}
+//
+//			buffer[bytesRecibidos] = '\0';
+//			printf("Me llegaron %d bytes con %s\n", bytesRecibidos, buffer);
+//		}
+//
+		free(buffer);
+
+		return 0;
 	}
-	log_info(console_log, "Se cargó el setup del COORDINADOR");
-
-	log_info(console_log, "");
-
-	log_info(console_log, "\tPuerto conecciones: %d",
-			coordinador_setup.PUERTO_ESCUCHA_CONEXIONES);
-
-	switch (coordinador_setup.ALGORITMO_DISTRIBUCION) {
-	case LSU:
-		log_info(console_log, "\tAlgoritmo de distribucion: LSU");
-		break;
-	case EL:
-		log_info(console_log, "\tAlgoritmo de distribucion: EL");
-		break;
-	case KE:
-		log_info(console_log, "\tAlgoritmo de distribucion: KE");
-		break;
-	}
-
-	log_info(console_log, "\tCantidad de entradas: %d",
-			coordinador_setup.CANTIDAD_ENTRADAS);
-
-	log_info(console_log, "\tTamanio de entrada en bytes: %d",
-			coordinador_setup.TAMANIO_ENTRADA_BYTES);
-
-	log_info(console_log, "\tRetardo en milis: %d",
-			coordinador_setup.RETARDO_MS);
-
-	log_info(console_log, "Se cerro la conexion con el planificador");
-	printf("\n\t\e[31;1m Consola terminada. \e[0m\n");
-
-	log_destroy(console_log);
-	return 0;
-}
+//	console_log = log_create("coordinador.log", "ReDistinto-Coordinador",
+//	true, LOG_LEVEL_TRACE);
+//	printf("\n\t\e[31;1m=========================================\e[0m\n");
+//	printf("\t.:: Bievenido a ReDistinto ::.");
+//	printf("\n\t\e[31;1m=========================================\e[0m\n\n");
+//	if (readConfig(PATH_FILE_NAME) < 0) {
+//		log_error(console_log, "No se encontró el archivo de configuración");
+//		return -1;
+//	}
+//	log_info(console_log, "Se cargó el setup del COORDINADOR");
+//
+//	log_info(console_log, "");
+//
+//	log_info(console_log, "\tPuerto conecciones: %d",
+//			coordinador_setup.PUERTO_ESCUCHA_CONEXIONES);
+//
+//	switch (coordinador_setup.ALGORITMO_DISTRIBUCION) {
+//	case LSU:
+//		log_info(console_log, "\tAlgoritmo de distribucion: LSU");
+//		break;
+//	case EL:
+//		log_info(console_log, "\tAlgoritmo de distribucion: EL");
+//		break;
+//	case KE:
+//		log_info(console_log, "\tAlgoritmo de distribucion: KE");
+//		break;
+//	}
+//
+//	log_info(console_log, "\tCantidad de entradas: %d",
+//			coordinador_setup.CANTIDAD_ENTRADAS);
+//
+//	log_info(console_log, "\tTamanio de entrada en bytes: %d",
+//			coordinador_setup.TAMANIO_ENTRADA_BYTES);
+//
+//	log_info(console_log, "\tRetardo en milis: %d",
+//			coordinador_setup.RETARDO_MS);
+//
+//	log_info(console_log, "Se cerro la conexion con el planificador");
+//	printf("\n\t\e[31;1m Consola terminada. \e[0m\n");
+//
+//	log_destroy(console_log);
--- Instancia/src/Instancia.c
+++ Instancia/src/Instancia.c
@@ -1,156 +1,184 @@
 #include "Instancia.h"
-
-void print_header() {
-	printf("\n\t\e[31;1m=========================================\e[0m\n");
-	printf("\t.:: Bienvenido a ReDistinto ::.");
-	printf("\t.:: Instancia -  ::.");
-	printf("\n\t\e[31;1m=========================================\e[0m\n\n");
-}
-
-void print_goodbye() {
-	printf("\n\t\e[31;1m=========================================\e[0m\n");
-	printf("\t.:: Gracias por utilizar ReDistinto ::.");
-	printf("\n\t\e[31;1m=========================================\e[0m\n\n");
-}
-
-void exit_program(int entero) {
-
-	if (console_log != NULL)
-		log_destroy(console_log);
-	if (coordinator_socket != 0)
-		close(coordinator_socket);
-
-	liberar_memoria();
-
-	printf("\n\t\e[31;1m FINALIZA INSTANCIA \e[0m\n");
-	exit(entero);
-}
-
-void create_log() {
-
-	console_log = log_create("instancia.log", "ReDistinto-Instancia", true,
-			LOG_LEVEL_TRACE);
-
-	if (console_log == NULL) {
-		printf(" FALLO - Creacion de Log");
-		exit_program(EXIT_FAILURE);
-	}
-}
-
-void loadConfig() {
-
-	log_info(console_log, " Cargan datos del archivo de configuracion");
-
-	t_config *config = config_create(PATH_FILE_NAME);
-
-	if (config == NULL) {
-		log_error(console_log,
-				"FALLO - No se encontro la configuracion del log");
-		exit_program(EXIT_FAILURE);
-	}
-
-	if (config != NULL) {
-		instancia_setup.IP_COORDINADOR = config_get_string_value(config,
-				"IP_COORDINADOR");
-		instancia_setup.PUERTO_COORDINADOR = config_get_int_value(config,
-				"PUERTO_COORDINADOR");
-		instancia_setup.ALGORITMO_REEMPLAZO = config_get_int_value(config,
-				"ALGORITMO_REEMPLAZO");
-		instancia_setup.PUNTO_MONTAJE = config_get_string_value(config,
-				"PUNTO_MONTAJE");
-		instancia_setup.NOMBRE_INSTANCIA = config_get_string_value(config,
-				"NOMBRE_INSTANCIA");
-		instancia_setup.INTERVALO_DUMP_SEGs = config_get_int_value(config,
-				"INTERVALO_DUMP_SEGs");
-
-		log_info(console_log, " Carga exitosa de archivo de configuracion");
-	}
-	config_destroy(config);
-}
-
-void liberar_memoria() {
-	// ADD
-
-}
-
-void log_inicial_consola() {
-
-	log_info(console_log, "\tCOORDINADOR: IP: %s, PUERTO: %d",
-			instancia_setup.IP_COORDINADOR, instancia_setup.PUERTO_COORDINADOR);
-
-	switch (instancia_setup.ALGORITMO_REEMPLAZO) {
-	case CIRC:
-		log_info(console_log, "\tAlgoritmo de reemplazo: CIRC");
-		break;
-	case LRU:
-		log_info(console_log, "\tAlgoritmo de reemplazo: LRU");
-		break;
-	case BSU:
-		log_info(console_log, "\tAlgoritmo de planificacion: BSU");
-		break;
-	}
-
-	log_info(console_log, "\tPunto de montaje: %s",
-			instancia_setup.PUNTO_MONTAJE);
-	log_info(console_log, "\tNombre de la instancia: %s",
-			instancia_setup.NOMBRE_INSTANCIA);
-	log_info(console_log, "\tIntervalo de dump en segundos: %d",
-			instancia_setup.INTERVALO_DUMP_SEGs);
-
-}
-
-void connect_with_coordinator() {
-
-	if (getClientSocket(&coordinator_socket, instancia_setup.IP_COORDINADOR,
-			instancia_setup.PUERTO_COORDINADOR)) {
-		exit_program(EXIT_FAILURE);
-	}
-
-	log_info(console_log, "Conexion exitosa con Coordinador.");
-	do_handshake(coordinator_socket);
-
-}
-
-void do_handshake() {
-
-	char *clientMessage = "INSTANCIA";
-
-	log_trace(console_log, "Handshake con Coordinador");
-
-	if (send(coordinator_socket, clientMessage, strlen(clientMessage), 0) < 0) {
-		log_error(console_log, "Error en la comunicacion");
-		exit_program(EXIT_FAILURE);
-	}
-
-	char * mensaje_ok = malloc(50);
-
-	if (recv(socket, mensaje_ok, sizeof(mensaje_ok), 0) <= 0) {
-		log_error(console_log, "FALLO - No se pudo hacer el hanshake");
-		exit_program(EXIT_FAILURE);
-	}
-
-	log_info(console_log, "Hanshake OK: %s.", (*mensaje_ok));
-
-	free(mensaje_ok);
-	free(clientMessage);
-
-	close(socket);
-
-}
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+//void print_header() {
+//	printf("\n\t\e[31;1m=========================================\e[0m\n");
+//	printf("\t.:: Bienvenido a ReDistinto ::.");
+//	printf("\t.:: Instancia -  ::.");
+//	printf("\n\t\e[31;1m=========================================\e[0m\n\n");
+//}
+//
+//void print_goodbye() {
+//	printf("\n\t\e[31;1m=========================================\e[0m\n");
+//	printf("\t.:: Gracias por utilizar ReDistinto ::.");
+//	printf("\n\t\e[31;1m=========================================\e[0m\n\n");
+//}
+//
+//void exit_program(int entero) {
+//
+//	if (console_log != NULL)
+//		log_destroy(console_log);
+//	if (coordinator_socket != 0)
+//		close(coordinator_socket);
+//
+//	liberar_memoria();
+//
+//	printf("\n\t\e[31;1m FINALIZA INSTANCIA \e[0m\n");
+//	exit(entero);
+//}
+//
+//void create_log() {
+//
+//	console_log = log_create("instancia.log", "ReDistinto-Instancia", true,
+//			LOG_LEVEL_TRACE);
+//
+//	if (console_log == NULL) {
+//		printf(" FALLO - Creacion de Log");
+//		exit_program(EXIT_FAILURE);
+//	}
+//}
+//
+//void loadConfig() {
+//
+//	log_info(console_log, " Cargan datos del archivo de configuracion");
+//
+//	t_config *config = config_create(PATH_FILE_NAME);
+//
+//	if (config == NULL) {
+//		log_error(console_log,
+//				"FALLO - No se encontro la configuracion del log");
+//		exit_program(EXIT_FAILURE);
+//	}
+//
+//	if (config != NULL) {
+//		instancia_setup.IP_COORDINADOR = config_get_string_value(config,
+//				"IP_COORDINADOR");
+//		instancia_setup.PUERTO_COORDINADOR = config_get_int_value(config,
+//				"PUERTO_COORDINADOR");
+//		instancia_setup.ALGORITMO_REEMPLAZO = config_get_int_value(config,
+//				"ALGORITMO_REEMPLAZO");
+//		instancia_setup.PUNTO_MONTAJE = config_get_string_value(config,
+//				"PUNTO_MONTAJE");
+//		instancia_setup.NOMBRE_INSTANCIA = config_get_string_value(config,
+//				"NOMBRE_INSTANCIA");
+//		instancia_setup.INTERVALO_DUMP_SEGs = config_get_int_value(config,
+//				"INTERVALO_DUMP_SEGs");
+//
+//		log_info(console_log, " Carga exitosa de archivo de configuracion");
+//	}
+//	config_destroy(config);
+//}
+//
+//void liberar_memoria() {
+//	// ADD
+//
+//}
+//
+//void log_inicial_consola() {
+//
+//	log_info(console_log, "\tCOORDINADOR: IP: %s, PUERTO: %d",
+//			instancia_setup.IP_COORDINADOR, instancia_setup.PUERTO_COORDINADOR);
+//
+//	switch (instancia_setup.ALGORITMO_REEMPLAZO) {
+//	case CIRC:
+//		log_info(console_log, "\tAlgoritmo de reemplazo: CIRC");
+//		break;
+//	case LRU:
+//		log_info(console_log, "\tAlgoritmo de reemplazo: LRU");
+//		break;
+//	case BSU:
+//		log_info(console_log, "\tAlgoritmo de planificacion: BSU");
+//		break;
+//	}
+//
+//	log_info(console_log, "\tPunto de montaje: %s",
+//			instancia_setup.PUNTO_MONTAJE);
+//	log_info(console_log, "\tNombre de la instancia: %s",
+//			instancia_setup.NOMBRE_INSTANCIA);
+//	log_info(console_log, "\tIntervalo de dump en segundos: %d",
+//			instancia_setup.INTERVALO_DUMP_SEGs);
+//
+//}
+//
+//void connect_with_coordinator() {
+//
+//	if (getClientSocket(&coordinator_socket, instancia_setup.IP_COORDINADOR,
+//			instancia_setup.PUERTO_COORDINADOR)) {
+//		exit_program(EXIT_FAILURE);
+//	}
+//
+//	log_info(console_log, "Conexion exitosa con Coordinador.");
+//	do_handshake(coordinator_socket);
+//
+//}
+//
+//void do_handshake() {
+//
+//	char *clientMessage = "INSTANCIA";
+//
+//	log_trace(console_log, "Handshake con Coordinador");
+//
+//	if (send(coordinator_socket, clientMessage, strlen(clientMessage), 0) < 0) {
+//		log_error(console_log, "Error en la comunicacion");
+//		exit_program(EXIT_FAILURE);
+//	}
+//
+//	char * mensaje_ok = malloc(50);
+//
+//	if (recv(socket, mensaje_ok, sizeof(mensaje_ok), 0) <= 0) {
+//		log_error(console_log, "FALLO - No se pudo hacer el hanshake");
+//		exit_program(EXIT_FAILURE);
+//	}
+//
+//	log_info(console_log, "Hanshake OK: %s.", (*mensaje_ok));
+//
+//	free(mensaje_ok);
+//	free(clientMessage);
+//
+//	close(socket);
+//
+//}
 
 // INICIO DE PROCESO
 int main(void) {
+	struct sockaddr_in servidorConfig;
+	servidorConfig.sin_family = AF_INET;
+	servidorConfig.sin_addr.s_addr = INADDR_ANY;
+	servidorConfig.sin_port = htons(8080);
+//	struct sockaddr_in direcciónServidor;
+//	direcciónServidor.sin_family = AF_INET;
+//	direcciónServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+//	direcciónServidor.sin_port = htons(8080);
 
-	print_header();
-	create_log();
-	loadConfig();
-	log_inicial_consola();
-	connect_with_coordinator();
+	int cliente = socket(AF_INET, SOCK_STREAM, 0);
+	if (connect(cliente, (void*) &servidorConfig, sizeof(servidorConfig)) != 0) {
+		perror("No se pudo conectar");
+		return 1;
+	}
+	char buffer[1000];
+	recv(cliente, buffer, 1000, 0);
+	puts(buffer);
+//	while (1) {
+//		char mensaje[1000];
+//		scanf("%s", mensaje);
+//
+//		send(cliente, mensaje, strlen(mensaje), 0);
+//	}
 
-	// TODO
-
-	print_goodbye();
-	exit_program(EXIT_SUCCESS);
+//return 0;
+//	print_header();
+//	create_log();
+//	loadConfig();
+//	log_inicial_consola();
+//	connect_with_coordinator();
+//
+//	// TODO
+//
+//	print_goodbye();
+//	exit_program(EXIT_SUCCESS);
 
 	return 0;
 }
